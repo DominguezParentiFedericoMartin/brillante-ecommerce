0a181bd6e64d169c630509a192b4e7df2aa4a842
Federico Martín Dominguez Parenti <dominguezparenti.wd@gmail.com>
2023-06-20 10:59:36 -0300

    primeros archivos

---
+++ src/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar() {
+  return (
+    <header>
+      <ul>
+        <li>asa</li>
+        <li>sdad</li>
+        <li>dgafasfa</li>
+      </ul>
+    </header>
+  );
+}
+export default Navbar;
